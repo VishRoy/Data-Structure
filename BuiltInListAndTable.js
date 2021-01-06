4ea7034f4e404044d771899d7105462ad0961128
VishRoy <69618079+VishRoy@users.noreply.github.com>
2021-01-06 20:21:20 +0530

    Built In List And Table - Array and Object

---
+++ BuiltInListAndTable.js
@@ -0,0 +1,16 @@
+// Built-in List : Array
+
+const shoppingList = ['Milk', 'Bread', 'Juice', 'Fruits', 'Vegetables']
+
+
+// Built-in Table : Object
+
+const citizens = {
+  'abc1' : {
+    firstName : 'Vishakha',
+    lastName : 'Roy',
+  },
+  'abc2' :{
+    //
+  }
+}
