0e8c0ea77b0156d3b1536cc674ec2571f0992c9a
Vishakha Roy <69618079+VishRoy@users.noreply.github.com>
2021-01-08 11:38:26 +0530

    Queue using Linked List

---
+++ Queue-LinkedList.js
@@ -0,0 +1,21 @@
+import {LinkedList} from './LinkedList.js';
+
+class Queue {
+  constructor(){
+    this.list = new LinkedList()
+  }
+
+  enqueue(value){
+      this.list.append(value)
+  }
+
+  dequeue(){
+      return this.list.deleteHead()
+  }
+  isEmpty(){
+    return !this.list.head;
+  }
+  toArray(){
+     return this.list.toArray()
+  }
+}
